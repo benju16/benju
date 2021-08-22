9d60e3bbce8c119cdf0fa051803ee4ba15cb1635
YEISON DAVID BENJUMEA GUTIERREZ <yeisonbenjumea187359@correo.itm.edu.co>
2021-08-21 22:42:46 -0500

    Add entity

---
+++ benju.Common/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace benju.Common.Models
+{
+    class Todo
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+       
+    }
+}
+ 
---
+++ benju.Common/Responses/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace benju.Common.Responses
+{
+    class Response
+    {
+        public bool InSuccess { get; set; }
+
+        public String Messeage { get; set; }
+
+        public bool IsCompleted { get; set; }
+    }
+}
